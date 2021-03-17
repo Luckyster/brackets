1b8f4cb136643cb86f75ca08b0e85f62ee7153ac
Gleb Frolov <gleb@thewhitelabelagency.com>
2021-03-17 22:16:53 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function inStr(str, bracketsConfig){
+      let strOut = str;
+      for( let bracketTemplate of bracketsConfig){
+          let pos = strOut.indexOf(bracketTemplate[0] + bracketTemplate[1]);
+
+          if(pos !== -1){
+              strOut = strOut.slice(0, pos) + strOut.slice(pos + 2);
+              strOut = inStr(strOut, bracketsConfig);
+          }
+      }
+      return  strOut;
+  }
+
+  if (inStr(str, bracketsConfig) === ''){
+      return true;
+ } else {
+      return false;
+  }
 }
